59833a7753e154d015c1ea9d3eafba5d28b73010
uint82 <abrorhilmi6@gmail.com>
2025-07-17 20:11:03 +0700

    added toolbar section for tools button for easier use. added undo button and its functionality. added new package zustand.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEditorStore } from '@/store/use-editor-store';
+
 import { useEditor, EditorContent } from '@tiptap/react';
 
 import ImageResize from 'tiptap-extension-resize-image';
@@ -15,7 +17,33 @@ import Image from '@tiptap/extension-image';
 
 
 export const Editor = () => {
+  const { setEditor } = useEditorStore();
+
   const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px;",
